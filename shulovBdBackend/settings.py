b1273ddf918540a8da46c07d25c089adfbb680cb
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2025-02-03 03:07:01 +0600

    add register, getClient, delete and edit feature for clients

--- shulovBdBackend/settings.py
+++ shulovBdBackend/settings.py
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'Users',
     'clients',
-    'corsheaders'
 ]
 
 MIDDLEWARE = [
